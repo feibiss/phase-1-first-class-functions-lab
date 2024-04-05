1e87e0a0c430deba1ee9d35efbbc2f2831e10b47
feibiss <fabiansang@gmail.com>
2024-04-05 22:01:34 +0300

    new commit

--- index.js
+++ index.js
@@ -2,20 +2,18 @@
 
 const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
-// Functions to select first or last two drivers
+
 const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
 const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
 
-// Array containing driver selection functions
+
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-// Higher-order function to create fare multipliers
 const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
 
-// Functions to double or triple fares
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-// Function to select drivers based on selection function
+
 const selectDifferentDrivers = (drivers, selectionFunction) => 
   selectionFunction(drivers);
